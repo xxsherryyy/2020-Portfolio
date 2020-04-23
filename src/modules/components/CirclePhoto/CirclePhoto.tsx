a798579765c10197ffe5cd50f7d9a42e090faaee
Sherry Yang <sherryxxyang@gmail.com>
2020-04-23 00:36:56 -0500

    add lottie stayHome

--- src/modules/components/CirclePhoto/CirclePhoto.tsx
+++ src/modules/components/CirclePhoto/CirclePhoto.tsx
@@ -1,16 +1,28 @@
 
 import React from "react";
 import './CirclePhoto.scss';
+import Lottie from "react-lottie";
 
+const defaultOptions = {
+  loop: true,
+  autoplay: true,
+  animationData: require('assets/animations/stayHome.json'),
+  rendererSettings: {
+    preserveAsepctRation: 'xMidYMid slice'
+  }
+};
 const CirclePhoto = ({ url, backText }) => {
+
+
     return (
       <div className="circlePhoto">
         <div className="photo">
           <div className="photo__side photo__side--front" style={{ backgroundImage: `url(${url})` }} >
           </div>
           <div className="photo__side photo__side--back">
-            {/* <h3 className="heading-tertiary">"{backText}"</h3> */}
-            <h3 className="heading-tertiary">Click for dark mode</h3>
+            <Lottie  options={defaultOptions}
+                     />
+           
           </div>
         </div>
       </div>
